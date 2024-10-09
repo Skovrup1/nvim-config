1cbd9da53906819bbc2fa3a9671b1ebd374ab2ef
skovrup1 <40407100+Skovrup1@users.noreply.github.com>
2024-10-09 23:53:47 +0200

    new stuff

--- lua/configs/lsp.lua
+++ lua/configs/lsp.lua
@@ -25,10 +25,12 @@ lsp_zero.extend_lspconfig({
 require('mason').setup({})
 require('mason-lspconfig').setup({
     handlers = {
+        -- auto config
         function(server_name)
             require('lspconfig')[server_name].setup({})
         end,
 
+        -- manual config
         lua_ls = function()
             require('lspconfig').lua_ls.setup({
                 on_init = function(client)
@@ -39,32 +41,27 @@ require('mason-lspconfig').setup({
     }
 })
 
-local cmp = require('cmp')
-local cmp_action = lsp_zero.cmp_action()
+-- manual install & config
+require('lspconfig').hls.setup({
+    settings = {
+        haskell = {
+            cabalFormattingProvider = "cabalfmt",
+            formattingProvider = "stylish-haskell"
+        }
+    }
+})
 
--- this is the function that loads the extra snippets
--- from rafamadriz/friendly-snippets
-require('luasnip.loaders.from_vscode').lazy_load()
+local cmp = require('cmp')
 
 cmp.setup({
     sources = {
         { name = 'path' },
         { name = 'nvim_lsp' },
-        { name = 'luasnip', keyword_length = 2 },
         { name = 'buffer',  keyword_length = 3 },
-    },
-    window = {
-        --completion = cmp.config.window.bordered(),
-        --documentation = cmp.config.window.bordered(),
-    },
-    snippet = {
-        expand = function(args)
-            require('luasnip').lsp_expand(args.body)
-        end,
     },
     mapping = cmp.mapping.preset.insert({
         -- confirm completion item
-        ['<Enter>'] = cmp.mapping.confirm({ select = true }),
+        ['<Tab>'] = cmp.mapping.confirm({ select = true }),
 
         -- trigger completion menu
         ['<C-Space>'] = cmp.mapping.complete(),
@@ -73,32 +70,34 @@ cmp.setup({
         ['<C-u>'] = cmp.mapping.scroll_docs(-4),
         ['<C-d>'] = cmp.mapping.scroll_docs(4),
 
-        -- navigate between snippet placeholders
-        ['<C-f>'] = cmp_action.luasnip_jump_forward(),
-        ['<C-b>'] = cmp_action.luasnip_jump_backward(),
     }),
     -- note: if you are going to use lsp-kind (another plugin)
     -- replace the line below with the function from lsp-kind
-    formatting = lsp_zero.cmp_format({ details = true }),
+    formatting = lsp_zero.cmp_format({
+        max_width = 40
+    }),
+
+    autocomplete = false
 })
 
 local completion_enabled = false
 
 function Toggle_completion()
-  completion_enabled = not completion_enabled
-  if completion_enabled then
-    cmp.setup({
-      completion = {
-        autocomplete = { 'TextChanged' },
-      }
-    })
-  else
-    cmp.setup({
-      completion = {
-        autocomplete = false
-      }
-    })
-  end
+    completion_enabled = not completion_enabled
+
+    if completion_enabled then
+        cmp.setup({
+            completion = {
+                autocomplete = { 'TextChanged' },
+            }
+        })
+    else
+        cmp.setup({
+            completion = {
+                autocomplete = false
+            }
+        })
+    end
 end
 
 vim.api.nvim_set_keymap('n', '<leader>tc', '<cmd>lua Toggle_completion()<CR>', { noremap = true, silent = true })
